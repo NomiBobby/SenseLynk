eec4ff33d289fd4a883f910d78679d58d433a150
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-04-26 23:45:48 -0400

    got rid of syncfusion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,16 @@ import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import avatar from '../data/avatar.jpg';
 import { Notification } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({title, customFunc, icon, color, dotColor}) => (
-  <TooltipComponent content ={title} position='BottomCenter'>
     <button type="button" onClick={customFunc} style={{color}} className='relative text-xl rounded-full p-3 hover:bg-light-gray'>
       <span style={{background: dotColor}} className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'/>
       {icon}
     </button>
-  </TooltipComponent>
 );
 
 const Navbar = () => {
@@ -50,7 +47,6 @@ const Navbar = () => {
         <NavButton title="Cart" customFunc={()=>handleClick('cart')} color="blue" icon={<FiShoppingCart />}/>
         <NavButton title="Chat" dotColor="#03C9D7" customFunc={()=>handleClick('chat')} color="blue" icon={<BsChatLeft />}/>
         <NavButton title="Notification" dotColor="#03C9D7" customFunc={()=>handleClick('notification')} color="blue" icon={<RiNotification3Line />}/>
-        <TooltipComponent content='Profile' position='BottomCenter'>
           <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick('userProfile')}>
             <img className='rounded-full w-8 h-8' src={avatar}/>
             <p>
@@ -59,7 +55,6 @@ const Navbar = () => {
             </p>
             <MdKeyboardArrowDown className='text-gray-400 text-14'/>
           </div>
-        </TooltipComponent>
 
         {/* {isClicked.cart && <Cart />}
         {isClicked.chat && <Chat />}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { RiSignalTowerFill } from 'react-icons/ri';
 import { MdOutlineCancel } from 'react-icons/md';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
@@ -29,7 +28,6 @@ const Sidebar = () => {
             className = "items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
             <RiSignalTowerFill /> <span>GeoLynk</span>
           </Link>
-          <TooltipComponent content="Menu" position="BottomCenter">
             <button type='button' 
               onClick={() => setActiveMenu(
                 (prevActiveMenu) =>
@@ -38,7 +36,6 @@ const Sidebar = () => {
               className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
               <MdOutlineCancel/>
             </button>
-          </TooltipComponent>
         </div>
         <div className="mt-10">
           {links.map((item)=>(
