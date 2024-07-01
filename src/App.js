004f1141acf9a7435a7af79fe5b268b161b0eb55
Qiwei Mao <57202568+NomiBobby@users.noreply.github.com>
2024-06-30 22:59:17 -0400

    renamed home page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Navbar } from './components';
-import { Gateway } from './pages';
+import { Home } from './pages';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="flex w-full flex-col h-screen lg:overflow-hidden overflow-auto">
         <Navbar/>
         <div className='flex grow flex-col gap-4 p-4 md:gap-8 md:p-8'>
           <Routes>
-            <Route path="/" element={<Gateway />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
-      </div>
     </BrowserRouter>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,1 @@
-export { default as Gateway } from './Gateway';
+export { default as Home } from './Home';
